f11cdc36f18da5bd8eb72f9d77f487ec0961f1f7
samin <sychowdhury2001@gmail.com>
2026-07-25 19:25:09 -0400

    quality test ran and documentation generated with readme

--- dbt_shopify/models/marts/facts/fct_orders.sql
+++ dbt_shopify/models/marts/facts/fct_orders.sql
@@ -3,11 +3,15 @@ select
     customer_id,
     order_name,
     order_number,
+
+    cast(total_price as decimal(10,2)) as total_price,
+    cast(subtotal_price as decimal(10,2)) as subtotal_price,
+    cast(total_tax as decimal(10,2)) as total_tax,
+
+    cast(created_at as timestamp) as created_at,
+    cast(processed_at as timestamp) as processed_at,
+
     financial_status,
-    currency,
-    total_price,
-    subtotal_price,
-    total_tax,
-    created_at,
-    processed_at
+    currency
+
 from {{ ref('stg_orders') }}
